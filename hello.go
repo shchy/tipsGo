4e0094db1f5798a4c025c3c83a247b14c19832f1
shch <shch.pc@gmail.com>
2018-01-28 00:56:13 +0900

    build error

--- hello.go
+++ hello.go
@@ -8,11 +8,12 @@ func main() {
 	fmt.Println("hello")
 
 	for i := range []int{1, 2, 3} {
-		u := unko{Name: fmt.Sprintf("Name %d", i), Age: i}
+		u := Task{Name: fmt.Sprintf("Name %d", i), Age: i}
 		fmt.Println(u)
 	}
 }
 
+// Task 最小単位の作業
 type Task struct {
 	Name string
 	Age  int
